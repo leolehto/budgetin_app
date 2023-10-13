86eb952ba352c1c83663946be69fedc6ebe32c5a
Leo Lehto <leo.lehto4@gmail.com>
2023-10-13 19:50:07 +0300

    budjetin lisäys ja muokkaus

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons} from '@expo/vector-icons';  
+import HomePage from './HomePage';
+import Stats from './Stats';
+const Tab = createBottomTabNavigator();
 
+const screenOptions = ({ route }) => ({
+  tabBarStyle:{
+    backgroundColor: 'black',
+    height: 50
+  },
+
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+
+    if (route.name === 'Home') {
+      iconName = 'md-home';
+    } else if (route.name === 'Stats') {
+      iconName = 'md-stats-chart';
+    }
+
+    return <Ionicons name={iconName} size={size} color={'orange'} backgroundColor={'black'} />;
+  }
+});
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Tästä tulee harjoitustyö</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="Stats" component={Stats} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ Stats.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, FlatList, Picker, StyleSheet } from 'react-native';
+
+export default function StatScreen() {
+    return (
+      <View style={styles.container}>
+        <Text>Stats!</Text>
+      </View>
+    );
+  }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
